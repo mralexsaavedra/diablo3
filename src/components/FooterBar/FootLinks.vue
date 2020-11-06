5a628e45e6673b342651193ba1dfbb2c30a4065d
Alexander <mralexsaavedra@gmail.com>
2020-11-06 17:53:29 +0100

    rename some things

--- src/components/FooterBar/FootLinks.vue
+++ src/components/FooterBar/FootLinks.vue
@@ -3,7 +3,7 @@
     <b-nav small align="start">
       <b-nav-item :to="{name: 'Home'}">Home</b-nav-item>
       <b-nav-item :to="{name: 'About'}">About</b-nav-item>
-      <b-nav-item href="https://github.com/baumannzone/diablo3-vue-platzi" target="_blank">Github</b-nav-item>
+      <b-nav-item href="https://github.com/mralexsaavedra/diablo3" target="_blank">Github</b-nav-item>
     </b-nav>
   </div>
 </template>
